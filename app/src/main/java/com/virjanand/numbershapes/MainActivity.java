b838ad91ce9facb510a696c84e1ba4ee203e0150
Virjanand <virjanand@virjanand.com>
2019-01-16 05:59:55 +0100

    Added checks for triangular and square and Log result

--- app/src/main/java/com/virjanand/numbershapes/MainActivity.java
+++ app/src/main/java/com/virjanand/numbershapes/MainActivity.java
@@ -4,11 +4,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     public void clickCheckNumber(View view) {
         Log.i("Info", "Click");
+
+        EditText numberText = (EditText) findViewById(R.id.numberEditText);
+        int number = Integer.parseInt(numberText.getText().toString());
+
+        int sum = 0;
+        String message;
+        for (int i = 1; i < number; i++) {
+            sum += i;
+            if (sum == number) {
+                Log.i("Number is ", "triangular.");
+            }
+            if (i * i == number) {
+                Log.i("Number is ", "square");
+            }
+        }
+//        Log.i("Number is ", "neither triangular nor square.");
     }
 
     @Override
